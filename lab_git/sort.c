1c8bd7743ac27b1b79bd4faec1e3a6a4c35de47d
grgrv-egr <grgrv.egr@yandex.ru>
2025-03-09 20:09:13 +0700

    Добавлен файл sort.c

---
+++ lab_git/sort.c
@@ -0,0 +1,15 @@
+void selectSort(struct Student arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int k = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j].total > arr[k].total) {
+                k = j;
+            }
+        }
+        if (k != i) {
+            struct Student temp = arr[i];
+            arr[i] = arr[k];
+            arr[k] = temp;
+        }
+    }
+}
