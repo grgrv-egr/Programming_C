6350a112dd00e25fa8aabf0c2c532b05f004b47e
grgrv-egr <grgrv.egr@yandex.ru>
2025-03-16 15:52:45 +0700

    Добавил функцию main в sort.c

--- lab_git/sort.c
+++ lab_git/sort.c
@@ -1,17 +1,27 @@
-void selectSort(struct Student arr[], int n) {
+#include <stdio.h>
+
+void selectSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         int k = i;
         for (int j = i + 1; j < n; j++) {
-            if (arr[j].total > arr[k].total) {
+            if (arr[j] > arr[k]) {
                 k = j;
             }
         }
         if (k != i) {
-            struct Student temp = arr[i];
+            temp = arr[i];
             arr[i] = arr[k];
             arr[k] = temp;
         }
     }
 }
+
+int main() {
+    int arr[] = {4, 0};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    sort(arr, n);
+    return 0;
+}
+
 #комментарий
 # еще комментарий
